d3cc4a05d1883b9b64b860abd90969403042952f
Refreshe141 <157901232+Refreshe141@users.noreply.github.com>
2025-06-07 12:51:34 +0900

    자바 10장 패키지
    >패키지의 개념과 특징을 이해
    >패키지를 생성하고 사용하는 방법 알기
    >자바의 내장 패키지에 대해 알기
    10장에서 실습 한 내용들을 정리하고 복습하는 용도

---
+++ JAVA_ch10/EX_01.java
@@ -0,0 +1,15 @@
+package Ch10;
+
+public class EX_01 {
+	public int add(int a, int b) {
+		return a + b;
+		
+	}
+	public static void main(String[] args) {
+		EX_01 obj = new EX_01();
+		System.out.println(obj.add(20, 30));
+
+	
+	}
+
+}
